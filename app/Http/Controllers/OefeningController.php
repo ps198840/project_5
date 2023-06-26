c3a03eac2107011bfdb60ecf25de13d77df908cd
ps218545 <ps218545@summacollege.nl>
2023-06-26 09:09:21 +0200

    API v1.1

--- app/Http/Controllers/OefeningController.php
+++ app/Http/Controllers/OefeningController.php
@@ -29,7 +29,6 @@ class OefeningController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-
     {
         $validator = Validator::make($request->all(), [
             'naam' => 'required',
